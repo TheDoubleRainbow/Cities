f691534a340cc18765620a4e16144d39462db9f7
TheDoubleRainbow <16mb.com@gmail.com>
2017-08-18 20:46:07 +0300

    update 3

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,11 +12,43 @@ class History extends React.Component{
 	}
 }
 
+class Notification extends React.Component{
+	constructor(props) {
+		super(props);
+		this.delay = false;
+		this.notification = document.getElementById('notification');
+	}
+
+	animate(){
+		if(!this.delay){
+			this.delay = true;
+			let that = this;
+			this.notification.style.display = 'block';
+			$("#notification").animateCss('flipInX');
+			setTimeout(function() {
+				$("#notification").animateCss('flipOutX')
+				//
+				console.log('out')
+				//
+				that.delay = false;
+				setTimeout(function() {that.notification.className = 'col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2'; that.notification.style.display = 'none';}, 700);
+			}, 3000);
+		}
+	}
+	render(){
+		this.animate()
+		return(
+				<div>{this.props.text}</div>
+			)
+	}
+}
+
 class Input extends React.Component{
 	constructor(props){
 		super(props);
+		this.started = false
 		this.text = '',
-		this.letter = 'Н',
+		this.letter = '',
 		this.setText = this.setText.bind(this),
 		this.submit = this.submit.bind(this)
 		this.emitSubmit = this.emitSubmit.bind(this)
@@ -36,7 +68,6 @@ class Input extends React.Component{
 	}
 
 	setText(e) {
-		console.log(this.props)
 		let text = e.target.value
 		if(text != ''){
 			let test = text.match(/[a-z,A-Z,А-Я,а-я,\-,і]/gi);
@@ -47,13 +78,13 @@ class Input extends React.Component{
 				}
 				else{
 					e.target.value = this.text;
-					this.notificate("Вводите только буквы, а также знак '-'")
+					ReactDOM.render(<Notification text='Вводите только буквы, а также знак "-"' />, document.getElementById('notification'))
 				}
 			}
 			else{
 				e.target.value = '';
 				this.text = '';
-				this.notificate("Вводите только буквы, а также знак '-'")
+				ReactDOM.render(<Notification text='Вводите только буквы, а также знак "-"' />, document.getElementById('notification'))
 			}
 			
 		}
@@ -66,29 +97,57 @@ class Input extends React.Component{
 		}
 		data.cities[8][0] = newCity;
 		data.history.push(newCity);
-		console.log(data)
 	}
 
 	submit() {
-		if(this.text != '' && this.text != '-'){
-			if(this.text[0].toUpperCase() == this.letter && this.text.length > 1){
-				let isRepeat = false;
-				for(var i = 0; i < data.history.length; i++){
-					if(data.history[i] == this.text) isRepeat = true
+		if(!this.started){
+			if(this.text != '' && this.text != '-'){
+				if(this.text.length > 1){
+					let isRepeat = false;
+					for(var i = 0; i < data.history.length; i++){
+						if(data.history[i] == this.text) isRepeat = true
+					}
+					if(!isRepeat){
+						this.updateData(this.text);
+						this.setLetter(this.text);
+						document.getElementById('inputText').value = '';
+						ReactDOM.render(<History />, document.getElementById('history'));
+						this.started = true;
+					}
 				}
-				if(!isRepeat){
-					this.updateData(this.text);
-					this.setLetter(this.text);
-					document.getElementById('inputText').value = '';
-					ReactDOM.render(<History />, document.getElementById('history'));
+				else{
+					ReactDOM.render(<Notification text='Введите корректный город' />, document.getElementById('notification'))
 				}
 			}
-			this.notificate("Город должен начинаться с буквы '" + this.letter + "'");
+			else{
+				ReactDOM.render(<Notification text='Поле не должно быть пустым' />, document.getElementById('notification'))
+			}
 		}
 		else{
-			this.notificate("Поле не должно быть пустым")
+			if(this.text != '' && this.text != '-'){
+				if(this.text[0].toUpperCase() == this.letter && this.text.length > 1){
+					let isRepeat = false;
+					for(var i = 0; i < data.history.length; i++){
+						if(data.history[i] == this.text) isRepeat = true
+					}
+					if(!isRepeat){
+						this.updateData(this.text);
+						this.setLetter(this.text);
+						document.getElementById('inputText').value = '';
+						ReactDOM.render(<History />, document.getElementById('history'));
+					}
+					else{
+						ReactDOM.render(<Notification text='Такой город уже был!' />, document.getElementById('notification'))
+					}
+				}
+				else{
+					ReactDOM.render(<Notification text={"Город должен начинаться с буквы '" + this.letter + "'"} />, document.getElementById('notification'))
+				}
+				}
+			else{
+				ReactDOM.render(<Notification text="Поле не должно быть пустым" />, document.getElementById('notification'))
+			}
 		}
-
 	}
 
 	emitSubmit(e){
@@ -97,34 +156,10 @@ class Input extends React.Component{
 		}
 	}
 
-	notificate(text){
-		var delay = false;
-		var notification = document.getElementById('notification');
-		ReactDOM.render(<div>{text}</div>, notification)
-		if(!delay){
-			delay = true
-
-			$("#notification").animateCss('slideInDown')
-			notification.style.display = 'block'
-			setTimeout(function() {
-				$("#notification").animateCss('flipOutX')
-				setTimeout(function() {notification.style.display = 'none'; delay = false}, 700);
-			}, 3000);
-		}
-	}
-
-	start(){
-		const startDiv = (
-			<div>
-				<div id="mainCity">Введите начальный город:</div>
-				<input />
-			</div>
-		)
-	}
 
 	render(){
 		return (
-			<div>
+			<div className="animated flipInX">
 				<div className="col-md-11 col-sm-11 col-xs-11 col-lg-11">
 						<div className="input-group">
 							<span className="input-group-addon">{this.letter}</span>
@@ -146,5 +181,6 @@ var data = {
 
 ReactDOM.render(<Input />, document.getElementById('input'));
 ReactDOM.render(<History />, document.getElementById('history'));
+ReactDOM.render(<Notification text='Введите первый город' />, document.getElementById('notification'))
 
 
